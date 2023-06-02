45c162afbdc957f34150f5dfb25deb8981a68bd8
Daniil Mikhailov <daniyanii@yandex.ru>
2023-06-02 13:53:31 +0300

    add on-change lib, add states for app class & main class

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ class App {
         {path: '', view: MainView}
     ]
 
+    appState = {
+        favorites: []
+    }
+     
     constructor() {
         window.addEventListener('hashchange', this.route.bind(this))
         this.route()
@@ -14,8 +18,9 @@ class App {
         if(this.currentView) {
             this.currentView.destroy()
         }
+        
         const view = this.routes.find(r => r.path === location.hash).view
-        this.currentView = new view()
+        this.currentView = new view(this.appState)
         this.currentView.render()
     }
 }
